bb310021fbce833807e301a18d790c54478bd8ed
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 06:40:59 +0400

    typo in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
-	output {
+	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
