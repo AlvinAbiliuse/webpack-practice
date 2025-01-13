cacb1bd68cb1633f0b800f1869197b6b83dae4e9
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-13 08:21:21 +0400

    moved image insertion from html to js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import image from "./images/image.png"
 
+const imEl = document.createElement("img");
+imEl.src = image;
+
+document.body.appendChild(image);
 console.log(greeting);
+
